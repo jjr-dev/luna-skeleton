37835177d2a06dcfa9a5ad16ee395bc83b64d2a3
Julimar Junior <julimargomesjr@hotmail.com>
2024-06-10 12:35:36 -0300

    Identação de arquivos

--- index.php
+++ index.php
@@ -1,25 +1,24 @@
 <?php
-    require __DIR__ . '/bootstrap.php';
+require __DIR__ . '/bootstrap.php';
 
-    // Load Dependencies
-    use Luna\Utils\View;
-    use Luna\Utils\Environment;
-    use Luna\Http\Middleware;
-    use Luna\Http\Router;
-    use Luna\Db\Database;
+// Load Dependencies
+use Luna\Utils\View;
+use Luna\Utils\Environment;
+use Luna\Http\Middleware;
+use Luna\Http\Router;
 
-    // Define URL Global Var
-    define("URL", Environment::get('URL'));
-    
-    // Define View Vars
-    View::define([
-        'URL' => URL,
-        'PUBLIC'  => URL . '/public'
-    ]);
+// Define URL Global Var
+define("URL", Environment::get('URL'));
 
-    // Define Middlewares
-    Middleware::setMap([]);
-    Middleware::setDefault([]);
+// Define View Vars
+View::define([
+    'URL' => URL,
+    'PUBLIC'  => URL . '/public'
+]);
 
-    // Start Routers
-    new Router(URL);
+// Define Middlewares
+Middleware::setMap([]);
+Middleware::setDefault([]);
+
+// Start Routers
+new Router(URL);
