05dcecca1d01784f80378213773d14118ef9cad1
Julimar Junior <julimargomesjr@hotmail.com>
2025-02-07 17:06:26 -0300

    Configurar CORS

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ use Luna\Utils\View;
 use Luna\Utils\Environment;
 use Luna\Http\Middleware;
 use Luna\Http\Router;
+use Luna\Http\Cors;
 
 // Define URL Global Var
 define("URL", Environment::get('URL'));
@@ -19,6 +20,13 @@ View::define([
 // Define Middlewares
 Middleware::setMap([]);
 Middleware::setDefault([]);
+
+// Define Cors
+Cors::setOrigins(["*"]);
+Cors::setMethods(["*"]);
+Cors::setHeaders(["*"]);
+Cors::setCredentials(true);
+Cors::setMaxAge(0);
 
 // Start Routers
 new Router(URL);
